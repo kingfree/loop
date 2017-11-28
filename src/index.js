6e52ee44eeabbe5990e91b09cc6d5762b960bd4e
tianjinfeng <tianjinfeng@91zdan.com>
2017-11-28 12:13:30 +0800

    顶部栏和底部栏，引入fa图标

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import dva from 'dva'
-import 'antd-mobile/dist/antd-mobile.css'  // or 'antd-mobile/dist/antd-mobile.less'
-import './index.css'
+import 'antd-mobile/dist/antd-mobile.less'
+import './styles/index.css'
 
 // 1. Initialize
 const app = dva()
--- src/routes/IndexPage.js
+++ src/routes/IndexPage.js
@@ -1,21 +1,105 @@
 import React from 'react'
 import { connect } from 'dva'
-import { WingBlank, WhiteSpace, Button, Badge } from 'antd-mobile'
+import { TabBar, NavBar } from 'antd-mobile'
+import { Icon } from 'react-fa'
 
-const repo = 'http://toyama.moe'
+class IndexPage extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      selectedTab: 'redTab',
+      hidden: false,
+      title: 'loop'
+    }
+  }
 
-function IndexPage() {
-  return (
-    <WingBlank>
-      <WhiteSpace size='lg'/>
-      <Button
-        type='primary'
-        onClick={() => location.href = repo}>
-        Ant design mobile web entry
-        <Badge text='new'/>
-      </Button>
-    </WingBlank>
-  )
+  renderContent(pageText) {
+    return (
+      <div style={{ backgroundColor: 'white', height: '100%', textAlign: 'center' }}>
+        <div style={{ paddingTop: 60 }}>Clicked “{pageText}” tab， show “{pageText}” information</div>
+      </div>
+    )
+  }
+
+  render() {
+    return (
+      <div
+        style={{ position: 'fixed', height: '100%', width: '100%', top: 0 }}>
+        <NavBar
+          mode="dark"
+          style={{ position: 'fixed', width: '100%', top: 0, zIndex: 9999, backgroundColor: "#cc154d" }}>
+          {this.state.title || 'loop'}</NavBar>
+        <TabBar
+          unselectedTintColor="#949494"
+          tintColor="#cc154d"
+          barTintColor="white"
+          hidden={this.state.hidden}
+        >
+          <TabBar.Item
+            title="首页"
+            key="Life"
+            badge={1}
+            icon={<Icon size="2x" name="music"/>}
+            selectedIcon={<Icon size="2x" name="music"/>}
+            selected={this.state.selectedTab === 'blueTab'}
+            onPress={() => {
+              this.setState({
+                selectedTab: 'blueTab'
+              })
+            }}
+            data-seed="logId"
+          >
+            {this.renderContent('Life')}
+          </TabBar.Item>
+          <TabBar.Item
+            title="场地"
+            key="Koubei"
+            badge={'new'}
+            icon={<Icon size="2x" name="users"/>}
+            selectedIcon={<Icon size="2x" name="users"/>}
+            selected={this.state.selectedTab === 'redTab'}
+            onPress={() => {
+              this.setState({
+                selectedTab: 'redTab'
+              })
+            }}
+            data-seed="logId1"
+          >
+            {this.renderContent('Koubei')}
+          </TabBar.Item>
+          <TabBar.Item
+            title="列表"
+            key="Friend"
+            dot
+            icon={<Icon size="2x" name="list"/>}
+            selectedIcon={<Icon size="2x" name="list"/>}
+            selected={this.state.selectedTab === 'greenTab'}
+            onPress={() => {
+              this.setState({
+                selectedTab: 'greenTab'
+              })
+            }}
+          >
+            {this.renderContent('Friend')}
+          </TabBar.Item>
+          <TabBar.Item
+            title="我的"
+            key="my"
+            icon={<Icon size="2x" name="user-circle"/>}
+            selectedIcon={<Icon size="2x" name="user-circle"/>}
+            selected={this.state.selectedTab === 'yellowTab'}
+            onPress={() => {
+              this.setState({
+                selectedTab: 'yellowTab'
+              })
+            }}
+          >
+            {this.renderContent('My')}
+          </TabBar.Item>
+        </TabBar>
+      </div>
+    )
+  }
 }
 
 IndexPage.propTypes = {}
